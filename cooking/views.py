a6d4eaa741c2106cfc8abe3241e573ed98365983
Bojchenko-Konstantin <Bojchenko_k@mail.ru>
2024-10-16 22:20:37 +0500

    feat: Add template, configure routes, create views.
     Add template Index, configure `conf/urls`,
     create `cooking/urls`, add view function `Index` to `cooking/views`.

--- cooking/views.py
+++ cooking/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Category, Post
 
-# Create your views here.
+def index(request):
+    """Функция представления для главной страницы"""
+    posts = Post.objects.all() # QuerySet
+    context = {
+            "title": "Главная страница",
+            "posts": posts
+            }
+    return render(request, "cooking/index.html", context)
+
+
