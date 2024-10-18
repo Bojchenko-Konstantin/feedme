a624d0f143998bf9244008d31882e73a2744fd41
Bojchenko-Konstantin <Bojchenko_k@mail.ru>
2024-10-19 00:07:38 +0500

    feat: Add new func - filter category post
    - Set up settings, update TIME_ZONE
    - Add new component - _category_buttons.html
    - Add new component - _post_list.html
    - Update index.html
    - Add new func in cooking/views (category_list)
    - Add new url in urls.py
    - Update base.html

--- cooking/views.py
+++ cooking/views.py
@@ -4,9 +4,22 @@ from .models import Category, Post
 def index(request):
     """Функция представления для главной страницы"""
     posts = Post.objects.all() # QuerySet
+    categories = Category.objects.all()
     context = {
             "title": "Главная страница",
-            "posts": posts
+            "posts": posts,
+            "categories": categories
+            }
+    return render(request, "cooking/index.html", context)
+
+def category_list(request, pk):
+    """Реакция на нажатие кнопки категории"""
+    posts = Post.objects.filter(category_id=pk)
+    categories = Category.objects.all()
+    context = {
+            "title": posts[0].category,
+            "posts": posts,
+            "categories": categories
             }
     return render(request, "cooking/index.html", context)
 
