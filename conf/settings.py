3cc24a295ebbd50195602e436b7e5584e1643723
Bojchenko-Konstantin <Bojchenko_k@mail.ru>
2024-10-19 14:57:34 +0500

    Update settings

--- conf/settings.py
+++ conf/settings.py
@@ -127,6 +127,7 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
